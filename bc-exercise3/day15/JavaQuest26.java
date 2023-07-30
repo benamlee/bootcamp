bb0343a5616941f0b770c85873df44bf08ea2040
benamlee <leehonamben@gmail.com>
2023-07-30 12:00:11 +0800

    day 15.1

--- bc-exercise3/day15/JavaQuest26.java
+++ bc-exercise3/day15/JavaQuest26.java
@@ -11,6 +11,8 @@ Constraints:
 1 <= nums[i] <= 100
 */
 
+import java.util.Arrays;
+
 public class JavaQuest26 {
   public static void main(String[] args) {
     System.out.println(sum(new int[] { 1, 2, 3, 2 })); // 4
@@ -21,25 +23,25 @@ public class JavaQuest26 {
 
   public static int sum(int[] nums) {
     // code here
-    int[] nums2=new int[nums.length]; // make a copy
-    for(int i=0;i<nums2.length;i++){
-      nums2[i]=nums[i];
-    }
-    int check=0; // all repeated numbers set 0
-    for(int i=0;i<nums2.length;i++){
-      check=nums2[i];
-      for(int j=0;j<nums2.length;j++){
-        if(i!=j&&nums2[j]==check){
-          nums2[j]=0;
-          nums2[i]=0;
+    // int[] nums2=new int[nums.length]; // make a copy
+    // for(int i=0;i<nums2.length;i++){
+    //   nums2[i]=nums[i];
+    // }
+    int[] nums2 = Arrays.copyOf(nums, nums.length); // make a copy
+    int check = 0; // all repeated numbers set 0
+    for (int i = 0; i < nums2.length; i++) {
+      check = nums2[i];
+      for (int j = 0; j < nums2.length; j++) {
+        if (i != j && nums2[j] == check) {
+          nums2[j] = 0;
+          nums2[i] = 0;
         }
       }
     }
-    check=0; // return sum of unique numbers
-    for(int i=0;i<nums2.length;i++){
-      check+=nums2[i];
+    check = 0; // return sum of unique numbers
+    for (int i = 0; i < nums2.length; i++) {
+      check += nums2[i];
     }
     return check;
   }
-
 }
